4c09770fda58b606f12b826a141cb47589af5da1
Marciel11 <marcielrodriguesd@gmail.com>
2023-09-28 10:37:22 -0300

    to do em react com typescript

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+
+      <h3>ToDo React</h3>
+
+    </header>
+  )
+}
+
+export default Header
